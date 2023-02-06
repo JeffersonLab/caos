89fc39eb1d780dff97594e56d634960a4105438d
gavalian <gavalian@jlab.org>
2023-02-06 15:03:07 -0500

    added a http server to the et monitoring code

--- java/data/src/main/java/org/jlab/data/h5/MonitorSourceEt.java
+++ java/data/src/main/java/org/jlab/data/h5/MonitorSourceEt.java
@@ -5,12 +5,17 @@
 package org.jlab.data.h5;
 
 import j4np.data.base.DataFrame;
+import j4np.hipo5.data.CompositeBank;
+import j4np.hipo5.data.CompositeNode;
 import j4np.hipo5.data.Event;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
+import twig.data.H1F;
+import twig.server.HttpDataServer;
+import twig.server.HttpServerConfig;
 
 /**
  *
@@ -22,11 +27,34 @@ public class MonitorSourceEt  {
     DataFrame<Event> etEvents = new DataFrame<>();
     DataSourceEt etSource = null;
     DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    HttpDataServer   httpServer = null;
+    private H1F hdcbank ;//= new H1F("");
+    CompositeNode cbank = new CompositeNode(120*1024);
     
     public MonitorSourceEt(String file, int interval){
         etSource = new DataSourceEt("localhost");
         etSource.open(file);
+        this.initServer();
+        
         this.initTimer(interval);
+    }
+    
+    public void initData(){
+        hdcbank = new H1F("hdcdata",120,0.0,800);
+        hdcbank.attr().set("fc=42,lc=2");
+        hdcbank.attr().setTitleX("Number Rows in DC bank");
+        hdcbank.attr().setTitleY("counts");
+        hdcbank.attr().setLegend("DC bank row multiplicity");
+        httpServer.getDirectory().add("/et/monitor", hdcbank);
+    }
+    
+    private void initServer(){
+         HttpServerConfig config = new HttpServerConfig();
+        config.serverPort = 8525;
+        
+        HttpDataServer.create(config);
+        HttpDataServer.getInstance().initDefault();
+        HttpDataServer.getInstance().start();
     }
     
     private void initTimer(int interval){
@@ -46,12 +74,19 @@ public class MonitorSourceEt  {
     
     private void onTimerEvent(){
         Date date = new Date();
+        //System.out.printf("%s: receive buffer with %d events\n",
+        //            dateFormat.format(date),etEvents.getCount());
+        
         try {
             etSource.nextFrame(etEvents);
             int size = 0;
-
             System.out.printf("%s: receive buffer with %d events\n",
                     dateFormat.format(date),etEvents.getCount());
+            for(int i = 0; i < etEvents.getCount(); i++){
+                Event e = (Event) etEvents.getEvent(i);
+                e.read(cbank, 42, 11);
+                hdcbank.fill(cbank.getRows());                
+            }
         } catch (Exception e){
             System.out.printf("%s: error receiving events from et ring.....\n",
                     dateFormat.format(date));
