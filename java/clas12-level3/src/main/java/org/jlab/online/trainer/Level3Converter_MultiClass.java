d9148af57f341ebd0180e18d6937bc36fa5017b8
rtysonCLAS12 <tyson@exppatricka-mbp.jlab.org>
2023-10-23 18:16:06 -0400

    First multiclass commit

--- java/clas12-level3/src/main/java/org/jlab/online/trainer/Level3Converter_MultiClass.java
+++ java/clas12-level3/src/main/java/org/jlab/online/trainer/Level3Converter_MultiClass.java
@@ -11,6 +11,8 @@ import j4np.hipo5.data.Node;
 import j4np.hipo5.io.HipoReader;
 import j4np.hipo5.io.HipoWriter;
 
+import java.util.ArrayList;
+import java.util.List;
 
 import twig.data.H1F;
 import twig.graphics.TGCanvas;
@@ -19,7 +21,7 @@ import twig.graphics.TGCanvas;
  *
  * @author gavalian
  */
-public class Level3Converter {
+public class Level3Converter_MultiClass {
     
     public static void analyzer(String file){
         
@@ -87,6 +89,25 @@ public class Level3Converter {
         }
         return trIndex;
     }
+
+    public static float getCALE(Bank CALBank, int pIndex){
+        float energy=0;
+        for (int row=0; row<CALBank.getRows();row++){
+            int trackPIndex=CALBank.getInt("pindex", row);
+            if (trackPIndex==pIndex){energy+=CALBank.getFloat("energy", row);}
+        }
+        return energy;
+    }
+
+    public static List<Integer> getPIndices_fSector(Bank TrackBank, int sector){
+        List<Integer> pIndices = new ArrayList<Integer>();
+        for (int row=0; row< TrackBank.getRows();row++){
+            int trackPIndex=TrackBank.getInt("pindex", row);
+            int trSector=TrackBank.getInt("sector", row);
+            if (trSector==sector){pIndices.add(trackPIndex);}
+        }
+        return pIndices;
+    }
     
     public static void convertDC(Bank bank, CompositeNode node, int... sectors){
         node.setRows(0);
@@ -122,12 +143,10 @@ public class Level3Converter {
         Event e = new Event();
         
         Bank[] banks = r.getBanks("DC::tdc","ECAL::adc","RUN::config");
-        Bank[]  dsts = r.getBanks("REC::Particle","REC::Track");
+        Bank[]  dsts = r.getBanks("REC::Particle","REC::Track","REC::Calorimeter");
         
         CompositeNode nodeDC = new CompositeNode( 12, 1,  "bbsbil", 4096);
         CompositeNode nodeEC = new CompositeNode( 11, 2, "bbsbifs", 4096);
-        CompositeNode nodeRC = new CompositeNode(  5, 1,  "b", 10);
-        CompositeNode nodeET = new CompositeNode(  5, 2,  "b", 10);
         
         while(r.hasNext()){
             
@@ -136,110 +155,88 @@ public class Level3Converter {
             
             e.read(dsts);
 
-            int pid = dsts[0].getInt(0, 0);
-            int status = dsts[0].getInt("status", 0);
-            int partSector = 0;
+            //V3
 
-            //only want particles in FD
-            if (Math.abs(status) >= 2000 && Math.abs(status) < 4000) {
-                // always taking first particle from event ie pIndex 0
-                int trIndex = Level3Converter.getTrackIndex(dsts[1], 0);
-                // trIndex==-1 means that pIndex not found in track bank
-                if (trIndex != -1) partSector = dsts[1].getInt("sector", trIndex);
+            //loop over sectors
+            for (int sect = 1; sect < 7; sect++) {
+                // get a list of pIndices in the sector
+                List<Integer> pIndices = getPIndices_fSector(dsts[1], sect);
 
-                //only want particles associated to a sector
-                if (partSector != 0) {
-                    long bits = banks[2].getLong("trigger", 0);
-
-                    int[] trigger = Level3Converter.convertTriggerLong(bits);
-
-                    int trigSector = Level3Converter.getTriggerSector(trigger);
-
-                    // if(trigger[0]>0) System.out.printf("%6d %4d %4d, %s\n",pid, trigSector,
-                    // partSector ,Arrays.toString(trigger));
-
-                    int[] labels = new int[] { pid, trigSector, partSector };
-
-                    // want to get DC and EC from the particle sector
-                    // not trigger sector
-                    Level3Converter.convertDC(banks[0], nodeDC, partSector);
-                    Level3Converter.convertEC(banks[1], nodeEC, partSector);
-
-                    // nodeDC.print();
-                    // nodeEC.print();
-
-                    Node tnode = new Node(5, 4, labels);
-
-                    if (nodeEC.getRows() > 0 && nodeDC.getRows() > 0) {
-                        e.write(nodeEC);
-                        e.write(nodeDC);
-                        e.write(tnode);
-                        w.addEvent(e);
+                //pid=-1 denotes no track in sector
+                int pid=-1;
+                float nEdep=0;
+                //check if there's at least 1 track in sector
+                if (pIndices.size() > 0) {
+                    pid = dsts[0].getInt(0, 0);
+                    
+                    //loop over particle indices in sector to check if there's an electron in sector
+                    for (int i = 0; i < pIndices.size(); i++) {
+                        if (dsts[0].getInt(0, i) == 11) {
+                            pid = 11;
+                        }
+                        //check if we have neutral pids
+                        if (dsts[0].getInt(0, i) == 22 || dsts[0].getInt(0, i) == 2212 || dsts[0].getInt(0, i) == 0) {
+                            nEdep+=getCALE(dsts[2],i);
+                        }
                     }
                 }
+
+                //for each data entry we record trigger
+                //could help with some logic in training data
+                //eg requiring conventional trigger to be wrong
+                long bits = banks[2].getLong("trigger", 0);
+                int[] trigger = Level3Converter.convertTriggerLong(bits);
+                int trigSector = Level3Converter.getTriggerSector(trigger);
+
+                int[] labels = new int[] { pid, trigSector, sect };
+
+                // get DC and EC from the sector
+                Level3Converter.convertDC(banks[0], nodeDC, sect);
+                Level3Converter.convertEC(banks[1], nodeEC, sect);
+
+                Boolean hasNeutral=false;
+                //hasNeutral means there's one or more photon 
+                //with >1 GeV E dep in calorimeters in total
+                if(nEdep>1){hasNeutral=true;}
+
+                //tag 0 is trash
+                int tag=0;
+
+                //tag=1 means there's an electron
+                if(pid==11){tag=1;}
+
+                //tag 2 means there's a negative pion but no neutral
+                //biggest source of bg for electrons
+                if(pid==-211 && !hasNeutral){tag=2;}
+                
+                //tag 3 means there's another particle (not el or pi-) but no neutral
+                if(pid!=11 && pid!=-211 && !hasNeutral){tag=3;}
+
+                 //tag 4 means there's a pion and neutral
+                if(pid==-211 && hasNeutral){tag=4;}
+
+                //tag 5 means there's another particle (not el not pion) and neutral
+                if(pid!=11 && pid!=-211 && hasNeutral){tag=4;}
+                
+                //tag 6 means there's no track and a neutral
+                if(pid==-1 && hasNeutral){tag=6;}
+
+                // nodeDC.print();
+                // nodeEC.print();
+
+                Node tnode = new Node(5, 4, labels);
+
+                if (nodeEC.getRows() > 0 && nodeDC.getRows() > 0 && tag>0) {
+                    e.write(nodeEC);
+                    e.write(nodeDC);
+                    e.write(tnode);
+
+                    e.setEventTag(tag);
+                    w.addEvent(e);
+                }
+
+
             }
-
-            /*
-            if(pid==11&&Math.abs(status)>=2000&&Math.abs(status)<3000){
-        
-                int[] electron = new int[]{0,0,0,0,0,0,0};
-                
-                int pindex = dsts[1].getInt("pindex", 0);
-                int sector = dsts[1].getInt("sector", 0);
-                
-                if(pindex==0) { electron[0] = 1; electron[sector] = 1;}
-                
-                nodeEC.setRows(0);
-                nodeDC.setRows(0);
-                nodeRC.setRows(0);
-                nodeET.setRows(0);
-                
-                int nrowsec = banks[1].getRows();
-                
-               // if(banks[2].getRows()>0){
-               //     int[] trigger = new int[7];
-               //     long     bits = banks[2].getLong("trigger", 0);
-               //     for(int i = 0; i < trigger.length; i++) {
-               //        trigger[i] = 0;
-               //         if( ((bits>>i)&(1L)) != 0L) trigger[i] = 1;
-               //         //System.out.println(Arrays.toString(trigger));
-               //     }
-               //     nodeRC.setRows(7); nodeET.setRows(7);
-               //     for(int i = 0; i < 7; i++) {
-               //         nodeRC.putByte(0, i, (byte) trigger[i]);
-               //         nodeET.putByte(0, i, (byte) electron[i]);
-               //     }
-               //}
-                if(nrowsec<4000){
-                    nodeEC.setRows(nrowsec);
-                    for(int row = 0; row < nrowsec; row++){
-                        nodeEC.putByte(  0, row, (byte) banks[1].getInt("sector", row));
-                        nodeEC.putByte(  1, row, (byte) banks[1].getInt("layer", row));
-                        nodeEC.putShort( 2, row, (short) banks[1].getInt("component", row));
-                        nodeEC.putByte(  3, row, (byte) banks[1].getInt("order", row));
-                        nodeEC.putInt(   4, row,  banks[1].getInt("ADC", row));
-                    }
-                } 
-                
-                int nrowsdc = banks[0].getRows();
-                if(nrowsdc<4000){
-                    nodeDC.setRows(nrowsdc);
-                    for(int row = 0; row < nrowsdc; row++){
-                        nodeDC.putByte(  0, row, (byte) banks[0].getInt("sector", row));
-                        nodeDC.putByte(  1, row, (byte) banks[0].getInt("layer", row));
-                        nodeDC.putShort( 2, row, (short) banks[0].getInt("component", row));
-                        nodeDC.putByte(  3, row, (byte) banks[0].getInt("order", row));
-                        nodeDC.putInt(   4, row,  banks[0].getInt("TDC", row));
-                    }
-                }
-                if(nodeEC.getRows()>0) e.write(nodeEC);
-                if(nodeDC.getRows()>0) e.write(nodeDC);
-                if(nodeRC.getRows()>0) e.write(nodeRC);
-                if(nodeET.getRows()>0) e.write(nodeET);
-                //int trigger = nodeRC.getInt(0, 0);
-                //if(trigger>0)
-                w.addEvent(e);
-            }*/
         }
         
         w.close();
@@ -280,7 +277,7 @@ public class Level3Converter {
             if(file>9){zeros="000";}
             if((file+4)>9){zeros2="000";}
             String fName=dir+base+zeros+fileS+"-"+zeros2+fileS2+".hipo";
-            Level3Converter.convertFile(fName, dir+"daq_"+fileS+".h5");
+            Level3Converter.convertFile(fName, dir+"daq_MC_"+fileS+".h5");
 
         }
 
---
+++ java/clas12-level3/src/main/java/org/jlab/online/trainer/Level3Trainer_MultiClass.java
@@ -0,0 +1,246 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.jlab.online.trainer;
+
+import j4np.hipo5.data.CompositeNode;
+import j4np.hipo5.data.Event;
+import j4np.hipo5.data.Node;
+import j4np.hipo5.io.HipoReader;
+import j4np.utils.io.OptionParser;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.jlab.online.level3.Level3Utils;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import twig.data.GraphErrors;
+import twig.server.HttpDataServer;
+import twig.server.HttpServerConfig;
+
+/**
+ *
+ * @author gavalian
+ */
+public class Level3Trainer_MultiClass {
+
+    ComputationGraph network = null;
+    public int nEpochs = 25;
+    public String cnnModel = "0a";
+
+    public Level3Trainer_MultiClass() {
+
+    }
+
+    public void initNetwork() {
+        ComputationGraphConfiguration config = Level3Models.getModel(cnnModel);
+        network = new ComputationGraph(config);
+        network.init();
+        System.out.println(network.summary());
+    }
+
+    public void save(String file) {
+
+        try {
+            network.save(new File(file + "_" + cnnModel + ".network"));
+            System.out.println("saved file : " + file + "\n" + "done...");
+        } catch (IOException ex) {
+            Logger.getLogger(Level3Trainer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void load(String file) {
+        try {
+            network = ComputationGraph.load(new File(file), true);
+            System.out.println(network.summary());
+        } catch (IOException ex) {
+            Logger.getLogger(Level3Trainer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+
+    public void evaluateFile(String file, int nEvents) {
+
+        INDArray[] inputs = this.getFromFile(file, nEvents);
+
+        INDArray[] outputs = network.output(inputs[0], inputs[1]);
+
+        long nTestEvents = inputs[0].shape()[0];
+
+        // System.out.println("Number of Test Events "+nTestEvents);
+        Level3Metrics metrics = new Level3Metrics(nTestEvents, outputs[0], inputs[2]);
+
+    }
+
+    public void trainManyFiles(List<String> files, int nEvents) {
+
+        // INDArray[] inputs = this.getFromFile(files.get(0), nEvents);
+
+        INDArray[] inputs = new INDArray[3];
+        int count = 0;
+        for (String file : files) {
+            INDArray[] inputs_temp = this.getFromFile(file, nEvents);
+            if (count == 0) {
+                inputs[0] = inputs_temp[0];
+                inputs[1] = inputs_temp[1];
+                inputs[2] = inputs_temp[2];
+            } else {
+                inputs[0] = Nd4j.vstack(inputs[0], inputs_temp[0]);
+                inputs[1] = Nd4j.vstack(inputs[1], inputs_temp[1]);
+                inputs[2] = Nd4j.vstack(inputs[2], inputs_temp[2]);
+            }
+        }
+
+        HttpServerConfig config = new HttpServerConfig();
+        config.serverPort = 8525;
+        HttpDataServer.create(config);
+        GraphErrors graph = new GraphErrors("graph");
+
+        HttpDataServer.getInstance().getDirectory().add("/server/training", graph);
+        HttpDataServer.getInstance().start();
+
+        // HttpDataServer.getInstance().getDirectory().list();
+        HttpDataServer.getInstance().getDirectory().show();
+
+        for (int i = 0; i < nEpochs; i++) {
+            long then = System.currentTimeMillis();
+            network.fit(new INDArray[] { inputs[0], inputs[1] }, new INDArray[] { inputs[2] });
+            long now = System.currentTimeMillis();
+            System.out.printf(">>> network iteration %8d, score = %e, time = %12d\n",
+                    i, network.score(), now - then);
+            graph.addPoint(i, network.score());
+            /*if (i % 25 == 0 && i != 0) {
+                this.save("level3_model_" + this.cnnModel + "_" + i + "_epochs.network");
+            }*/
+        }
+    }
+
+
+    public INDArray[] getFromFile(String file, int max) {
+        HipoReader r = new HipoReader(file);
+
+        int nMax = max;
+
+        if (r.entries() < max)
+            nMax = r.entries();
+
+        CompositeNode nDC = new CompositeNode(12, 1, "bbsbil", 4096);
+        CompositeNode nEC = new CompositeNode(11, 2, "bbsbifs", 4096);
+
+        INDArray DCArray = Nd4j.zeros(nMax, 1, 6, 112);
+        INDArray ECArray = Nd4j.zeros(nMax, 1, 6, 72);
+        INDArray OUTArray = Nd4j.zeros(nMax, 2);
+        Event event = new Event();
+        int counter = 0;
+        int npos = 0;
+        int nneg = 0;
+        while (r.hasNext() == true && counter < nMax) {
+            r.nextEvent(event);
+            event.read(nDC, 12, 1);
+            event.read(nEC, 11, 2);
+
+            Node node = event.read(5, 4);
+
+            int[] ids = node.getInt();
+
+            // Do we care if the trigger is fired in the event? (ids[1]>0) - no
+            // do we care if trigger is right (&& ids[1] == ids[2])? - no
+            if (ids[0] == 11) {
+                // Want particle sector to be non null (ids[2]) for positive only
+                //if (ids[2] > 0 && npos<nneg) {
+                Level3Utils.fillDC(DCArray, nDC, ids[2], counter);
+                Level3Utils.fillEC(ECArray, nEC, ids[2], counter);
+                Level3Utils.fillLabels(OUTArray, 1, counter);
+                counter++;
+                npos++;
+                //}
+            } else {
+                // always have more neg than pos, balance dataset by only adding neg after pos
+                // do we care if trigger is wrong (&& ids[1] == ids[2])? - means training in
+                // worse case scenario
+                if (nneg < npos) {
+                    Level3Utils.fillDC(DCArray, nDC, ids[2], counter);
+                    Level3Utils.fillEC(ECArray, nEC, ids[2], counter);
+                    Level3Utils.fillLabels(OUTArray, 0, counter);
+                    counter++;
+                    nneg++;
+                }
+            }
+
+        }
+
+        System.out.printf("\n\n loaded samples (%d)  positive = %s, negative = %d\n", counter, npos, nneg);
+        return new INDArray[] { DCArray, ECArray, OUTArray };
+    }
+
+    public static void main(String[] args) {
+        int mode = -1;
+
+        if (mode > 0) {
+
+            Level3Trainer t = new Level3Trainer();
+            t.load("level3_model_0a_1000_epochs.network_0a.network");
+            // t.load("level3_model_0b_625_epochs.network_0b.network");
+            String file = "rec_clas_005197.evio.00405-00409.hipo_daq.h5";
+            t.evaluateFile(file, 10000);
+
+        } else if(mode<0){
+            //String baseLoc="/Users/tyson/data_repo/trigger_data/rga/daq_";
+            String baseLoc="/Users/tyson/data_repo/trigger_data/rgd/018437/daq_";
+            String net="0b";
+	        Level3Trainer t = new Level3Trainer();
+
+            List<String> files= new ArrayList<>();
+            for (int file=0;file<5;file+=5){
+                files.add(baseLoc+String.valueOf(file)+".h5");
+            }
+
+	        t.cnnModel = net;
+
+            //if not transfer learning
+	        //t.initNetwork();
+
+            //transfer learning
+            //t.load("level3_"+net+".network");
+
+	        t.nEpochs = 1500;
+	        //t.trainManyFiles(files,200000);//10
+	        //t.save("level3");
+	    
+	        String file2=baseLoc+"5.h5";
+
+	        t.load("level3_"+net+"_fCF.network");
+            //t.load("etc/networks/network-level3-0c-rgc.network");
+	        t.evaluateFile(file2,200000);
+
+        }else {
+
+            OptionParser parser = new OptionParser("trainer");
+
+            parser.addOption("-n", "level3", "output network name");
+            parser.addOption("-m", "0b", "model name (0a,0b or 0c)");
+            parser.addOption("-e", "1024", "number of epochs");
+            parser.addOption("-max", "200000", "number of data samples");
+
+            parser.parse(args);
+
+            String net = parser.getOption("-m").stringValue();
+            Level3Trainer t = new Level3Trainer();
+
+            t.cnnModel = net;
+            t.initNetwork();
+            t.nEpochs = parser.getOption("-e").intValue();
+            int max = parser.getOption("-max").intValue();
+
+            t.trainManyFiles(parser.getInputList(), max);
+            t.save(parser.getOption("-n").stringValue());
+
+        }
+    }
+}
